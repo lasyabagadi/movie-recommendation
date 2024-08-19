420464f1ec44dfa06f844475eb5f48f40ae3c1f7
lasyabagadi <lasyapriya4594@gmail.com>
2024-08-19 21:52:14 +0530

    commit

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
-import streamlit as st
+from flask import Flask, render_template, request
 import pickle
 import pandas as pd
 import requests
 
+app = Flask(__name__)
 
 def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=a5f40438209e351d7475e552b85358e7&language=en-US".format(
-        movie_id)
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=a5f40438209e351d7475e552b85358e7&language=en-US"
     data = requests.get(url).json()
     poster_path = data.get('poster_path')
     if poster_path:
         full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
         return full_path
-
 
 def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
@@ -26,25 +25,23 @@ def recommend(movie):
 
     return recommended_movie_names, recommended_movie_posters
 
-
+# Load the movie dictionary and similarity matrix
 movie_dict = pickle.load(open('movie_dict.pkl', 'rb'))
 movies = pd.DataFrame(movie_dict)
-
 similarity = pickle.load(open('similarity.pkl', 'rb'))
 
-st.title('Movie Recommender System')
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    selected_movie = None
+    recommended_movie_names = []
+    recommended_movie_posters = []
+    
+    if request.method == 'POST':
+        selected_movie = request.form.get('movie_name')
+        recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
+        
+    return render_template('index.html', movies=movies['title'].values, selected_movie=selected_movie or '', recommended_movie_names=recommended_movie_names, recommended_movie_posters=recommended_movie_posters, zip=zip)
 
-selected_moviename = st.selectbox(
-    'Select a movie to get recommendations',
-    movies['title'].values
-)
 
-if st.button('Recommend'):
-    recommended_movie_names, recommended_movie_posters = recommend(selected_moviename)
-
-    # Display all the posters in a single line with their names above
-    cols = st.columns(5)  # Create 5 columns for 5 recommendations
-    for i in range(len(recommended_movie_names)):
-        with cols[i]:
-            st.text(recommended_movie_names[i])  # Display the name above
-            st.image(recommended_movie_posters[i])  # Display the poster below the name
+if __name__ == '__main__':
+    app.run(debug=True)
